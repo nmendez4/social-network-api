02bc090a8aa4a2de5c078185defad715d7e5b11c
nmendez4 <nmendez6594@gmail.com>
2022-02-06 21:25:40 -0500

    final touches

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-    useFindAndModify: false,
     useNewURLParser: true,
     useUnifiedTopology: true
 });
 
-// use this to log mongo queries
+// use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`Connected on localhost: ${PORT}`));
